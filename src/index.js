fab1f6401c9602e408bca934495148d6917a711c
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2019-06-19 10:58:55 -0600

    minor import changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../public/index.html'
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
